62a1f97037b2644726eabbb4a236446ab23b4ec2
Alpheus Masanga <alklyn@gmail.com>
2016-07-09 22:47:14 +0200

    Initial Commit.

---
+++ media.py
@@ -0,0 +1,11 @@
+class Movie():
+    """
+    Class to store & display movie related info
+    """
+    pass
+
+    def __init__(self):
+        """
+        Intitialize variable
+        """
+        pass
